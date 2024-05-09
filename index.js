eb4bfbb557945601a14f2c117acad289fdf2f9aa
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-05-09 20:15:48 +0300

    server index

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { networkInterfaces } = require('os');
+const route = require('./routes');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+const port = process.env.PORT || 5678;
+
+app.listen(port, () => {
+    const interfaces = networkInterfaces();
+    let localIP;
+
+    Object.keys(interfaces).forEach(interfaceName => {
+        interfaces[interfaceName].forEach(interfaceInfo => {
+            if (interfaceInfo.family === 'IPv4' && !interfaceInfo.internal && interfaceInfo.address.startsWith('192.168.')) {
+                localIP = interfaceInfo.address;
+            }
+        });
+    });
+
+    if (!localIP) {
+        console.error("Couldn't find a 192.168.x.x IP address. Defaulting to localhost.");
+        localIP = 'localhost';
+    }
+
+    console.log("Server running at \n local host: http://localhost:" + port + "\n local network: http://" + localIP + ":" + port);
+});
+
+route(app);
+
