557f7a066e916a0f732e37d8f30d08daf1f5d07d
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-05-11 20:48:19 +0300

    news router

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const { newsById } = require('../controllers/news');
 
 const route = express.Router();
 
 route.get('/', (req, res)=>{
     res.render('news');
 })
+
+route.get('/id/:id/:title', newsById)
 
 module.exports = route;
