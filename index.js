d388c4f7d8764421cddfc05e8cb5cf071fc50d0c
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-05-11 20:44:25 +0300

    allow form submit

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 const port = process.env.PORT || 5678;
 
