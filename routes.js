774fa7b7c57adaa1fd1b6144fd5a73178ecca6e7
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-05-11 20:46:25 +0300

    other routes

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const aboutRoute = require('./routes/about');
 const newsRoute = require('./routes/news');
 const hiwRoute = require('./routes/hiw');
+const donateRoute = require('./routes/donate');
+const feedbackRoute = require('./routes/feedback');
 
 function route(app){
     
@@ -10,7 +12,11 @@ function route(app){
     app.use('/about', aboutRoute);
     app.use('/news', newsRoute);
     app.use('/hiw', hiwRoute);
-
+    app.use('/donate', donateRoute);
+    app.use('/feedback', feedbackRoute);
+    app.get('/success', (req, res)=>{
+        res.render('success');
+    })
     app.use((req, res)=>{
         res.status(404).render('404');
     })
